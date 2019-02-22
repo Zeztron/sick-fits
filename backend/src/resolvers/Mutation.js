a88619ab008d7adc52cebb621113b9265038b4b8
Zeztron <pateltharsh@gmail.com>
2019-02-22 11:05:28 -0500

    added item creation schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,23 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        // Create a dog
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
+    async createItem(parent, args, ctx, info) {
+        // Todo: check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
     }
+
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     // Create a dog
+    //     const newDog = { name: args.name };
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
 };
 
 module.exports = Mutations;
