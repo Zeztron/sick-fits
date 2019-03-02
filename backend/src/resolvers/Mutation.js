318aebdfba395831b07c490ed91e3b2a3f3244c7
Zeztron <pateltharsh@gmail.com>
2019-03-02 13:23:50 -0500

    added signout button

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -90,6 +90,14 @@ const Mutations = {
 
         // return user
         return user;
+    },
+
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token');
+
+        return { 
+            message: 'Goodbye!'
+        }
     }
 };
 
