a35c224d9006cfc5cae4b119298040018c3e0a56
Zeztron <pateltharsh@gmail.com>
2019-03-03 13:47:53 -0500

    created a gated signin component

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,10 +6,18 @@ const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-        // Todo: check if they are logged in
+        if(!ctx.request.userId) {
+            throw new Error("You must be logged in to do that");
+        }
 
         const item = await ctx.db.mutation.createItem({
             data: {
+                // This is how we create relation between the Item and the User
+                user: {
+                    connect: {
+                        id: ctx.request.userId
+                    }
+                },
                 ...args
             }
         }, info);
