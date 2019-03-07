40af5a8c08dfa05b3bfa0050b8ba4a6ba9fd783f
Zeztron <pateltharsh@gmail.com>
2019-03-07 17:05:24 -0500

    added cart to frontend

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
-import Nav from './Nav';
 import Link from 'next/link';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import Nav from './Nav';
+import Cart from './Cart';
 
 Router.onRouteChangeStart = () => {
     NProgress.start();
@@ -70,7 +71,7 @@ const Header = () => (
         <div className="sub-bar">
             <p>Search</p>
         </div>
-        <div>Cart</div>
+        <div><Cart/></div>
     </StyledHeader>
 )
 
