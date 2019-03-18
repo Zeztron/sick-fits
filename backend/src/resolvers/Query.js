2f892cbfd49ffa6d6cc952ddce33dcc3c314d295
Zeztron <pateltharsh@gmail.com>
2019-03-18 13:03:57 -0400

    can display single item order

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,8 +3,11 @@ const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
+
     item: forwardTo('db'),
+
     itemsConnection: forwardTo('db'),
+
     me(parent, args, ctx, info) {
         // check if there is a current userId
         if(!ctx.request.userId) {
@@ -14,6 +17,7 @@ const Query = {
             where: { id: ctx.request.userId },
         }, info);
     },
+
     async users(parent, args, ctx, info) {
         // Check if they are logged in
         if(!ctx.request.userId) {
@@ -25,7 +29,29 @@ const Query = {
 
         // If they do, query all the users
         return ctx.db.query.users({}, info);
+    },
+
+    async order(parent, args, ctx, info) {
+        // Make sure they are logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+
+        // Query the current order
+        const order = await ctx.db.query.order({
+            where: { id: args.id},
+        }, info)
+
+        // Check if they have the permissions to see this order
+        const ownsOrder = order.user.id === ctx.request.userId;
+        const hasPermissionToSeeOrder = ctx.request.user.permissions.includes("ADMIN");
+        if(!ownsOrder || !hasPermissionToSeeOrder) {
+            throw new Error("You can't see this order");
+        }
+        // Return the order
+        return order;
     }
+
 };
 
 module.exports = Query;
--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -29,6 +29,7 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
     onToken = async (res, createOrder) => {
+        NProgress.start();
         // console.log(res);
         // manually call the mutation once we have the stripe token
         const order = await createOrder({
@@ -38,8 +39,12 @@ class TakeMyMoney extends React.Component {
         }).catch(err => {
             alert(err.message);
         });
-        console.log(order);
-    }
+        // console.log(order);
+        Router.push({
+            pathname: '/order',
+            query: { id: order.data.createOrder.id }
+        });
+    };
 
     render() {
         return (
