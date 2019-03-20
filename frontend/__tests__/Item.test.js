1fe44d9a9e917a6df92224fba295fb9429b5774a
Zeztron <pateltharsh@gmail.com>
2019-03-20 10:34:32 -0400

    testing item

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,8 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+describe('<Item/>', () => {
+    it('renders and displays properly', () => {
+        // const wrapper = shallow(<ItemComponent/>);
+    });
+})
